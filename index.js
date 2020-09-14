97c465eaad8834764e0e8e56ceb01538fd2bea55
SarthakSri98 <sarthaksri1998@gmail.com>
2020-09-15 00:11:50 +0530

    feat: superhero powers added

--- index.js
+++ index.js
@@ -102,6 +102,45 @@ app.post('/', express.json() ,(req,res)=>{
     }
 
     intentMap.set("GetSuperheroIntro",getSuperHeroIntro);
+
+    function getSuperPowers(){
+        let responseJson = {};
+        // console.log(req.body);
+        console.log('Parameters:'+JSON.stringify(parameters));
+        // console.log('Input Contexts:'+JSON.stringify(inputContexts));
+        let superhero = parameters['superhero'];
+        let powers = 'unknown';
+        switch(superhero){
+            case 'Superman':{
+                powers = "super strength, flight, invulnerability, super speed, heat vision, freeze breath, x-ray vision, superhuman hearing, healing factor";
+                break;
+            }
+            case 'Batman':{
+                powers = "exceptional martial artist, combat strategy, inexhaustible wealth, brilliant deductive skill, advanced technology.";
+                break;
+            }
+            case 'Spiderman':{
+                powers = "wall-crawling, enhanced strength, speed, reflexes, durability, stamina, healing, and agility";
+                break;
+            }
+            case 'Hulk':{
+                powers = "incredible superhuman strength, durability, and healing factor";
+                break;
+            }
+            case 'Flash':{
+                powers = "super speed, intangibility, superhuman agility";
+                break;
+            }
+            default:{
+                powers = 'unknown';
+            }
+        }
+        responseJson.displayText = "The powers of "+superhero+" : "+ powers;
+        console.log('Response:'+JSON.stringify(responseJson));
+        agent.add(responseJson.displayText);
+    }
+
+    intentMap.set("GetSuperPowers",getSuperPowers);
     
     agent.handleRequest(intentMap);
 
